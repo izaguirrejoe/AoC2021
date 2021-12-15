f4a15f3d1fb33c676c44069c37c89ee55b60fe75
Joseph Izaguirre <izaguirrejoe@gmail.com>
2021-12-14 16:16:03 -0800

    Day 5

--- day5/day5.rb
+++ day5/day5.rb
@@ -22,5 +22,46 @@ lines.each do |line|
     end
   end
 end
+p map.flatten.compact.reduce(0) { |sum, num| sum += (num > 1) ? 1 : 0 }
+
+#Part 2
+map = []
+
+lines.each do |line|
+  x1,y1,x2,y2 = line.match(/(\d+),(\d+) -> (\d+),(\d+)/).captures.map(&:to_i)
+  a, b = [[x1,y1], [x2,y2]].sort
+  x1, y1 = a
+  x2, y2 = b
+  if x1 == x2 
+    map[x1] ||= Array.new
+    for y in y1..y2
+      map[x1][y] ||= 0
+      map[x1][y] += 1
+    end
+  elsif y1 == y2
+    for x in x1..x2
+      map[x] ||= Array.new
+      map[x][y1] ||= 0
+      map[x][y1] += 1
+    end
+  else
+    x = x1
+    y = y1
+
+    if y1 < y2
+      increment = 1
+    else
+      increment = -1
+    end
+
+    while x <= x2
+      map[x] ||= Array.new
+      map[x][y] ||= 0
+      map[x][y] += 1
+      x += 1
+      y += increment
+    end
+  end
+end
 
 p map.flatten.compact.reduce(0) { |sum, num| sum += (num > 1) ? 1 : 0 }
